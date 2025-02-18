95ee43863b3e0c26843904f24fc9c7a11fbbd48e
Stephen Bonnell <smbonn2005@hotmail.com>
2025-02-18 06:30:42 +0000

    refactor: use 1password for talos secrets and update folder paths

--- kubernetes/components/volsync/readme.md
+++ kubernetes/components/volsync/readme.md
@@ -25,7 +25,7 @@ apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 spec:
   components:
-    - ../../../../flux/components/volsync
+    - ../../../../components/volsync
 ```
 
 ## Required `postBuild` vars:
---
+++ scripts/cilium-config.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# shellcheck disable=SC2155
+export ROOT_DIR="$(git rev-parse --show-toplevel)"
+# shellcheck disable=SC1091
+source "$(dirname "${0}")/lib/common.sh"
+
+function wait_for_crds() {
+    local -r crds=(
+        "ciliuml2announcementpolicies" "ciliumloadbalancerippools"
+    )
+
+    for crd in "${crds[@]}"; do
+        until kubectl get crd "${crd}.cilium.io" &>/dev/null; do
+            log info "Cilium CRD is not available. Retrying in 10 seconds..." "crd=${crd}"
+            sleep 10
+        done
+    done
+}
+
+function apply_config() {
+    log debug "Applying Cilium config"
+
+    local -r cilium_config_dir="${ROOT_DIR}/kubernetes/apps/kube-system/cilium/config"
+
+    if [[ ! -d "${cilium_config_dir}" ]]; then
+        log fatal "No Cilium config directory found" "file=${cilium_config_dir}"
+    fi
+
+    if kubectl --namespace kube-system diff --kustomize "${cilium_config_dir}" &>/dev/null; then
+        log info "Cilium config is up-to-date"
+    else
+        if kubectl apply --namespace kube-system --server-side --field-manager kustomize-controller --kustomize "${cilium_config_dir}" &>/dev/null; then
+            log info "Cilium config applied successfully"
+        else
+            log fatal "Failed to apply Cilium config"
+        fi
+    fi
+}
+
+function main() {
+    wait_for_crds
+    apply_config
+}
+
+main "$@"
